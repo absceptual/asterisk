19e71c1ecb8e235ddf158506f0baa44ec55211ed
absceptual <absceptual@gmail.com>
2025-01-12 23:55:09 -0600

    rough draft but running java code from strings work

--- Main.java
+++ Main.java
@@ -26,7 +26,6 @@ class CompilationCodeObject extends SimpleJavaFileObject {
 }
 
 // After the bytecode is compiled, we want to send the bytecode to this class
-
 class CompilationObject extends SimpleJavaFileObject {
 
     protected final ByteArrayOutputStream bytecode;
@@ -47,14 +46,12 @@ class CompilationObject extends SimpleJavaFileObject {
     }
 }
 
-
-class CompilationManager extends ForwardingJavaFileManager {
+class CompilationManager<T> extends ForwardingJavaFileManager<JavaFileManager> {
     
     private CompilationObject classObject;
 
     public CompilationManager(StandardJavaFileManager manager) {
         super(manager);
-        
     }
 
     @Override 
@@ -77,63 +74,92 @@ class CompilationManager extends ForwardingJavaFileManager {
 
 class CodeExecution {
     private JavaCompiler compiler;
-    private CompilationManager manager;
+    private CompilationManager<JavaFileManager> manager;
     private StringWriter compilerOutput;
     private ByteArrayOutputStream programOutput;
-    private ByteArrayOutputStream programInput; 
+    private InputStream programInput;
     private ByteArrayOutputStream programErrors;
-
 
     public void compile(String fullname, String code) {
         compilerOutput = new StringWriter(32768);
-
-        
+       
         if (compiler == null) {
             System.out.println("gay!\n");
             return;
         }
-            
-            
+               
         // To create a new task our Java file objects have to be in a List<> 
         List<CompilationCodeObject> unit = new ArrayList<>();
         unit.add(new CompilationCodeObject(fullname, code));
         compiler.getTask(compilerOutput, manager, null, null, null, unit).call();
     }
 
-    public boolean execute(InputStream input, String fullname, String code) {
+    public boolean execute(String fullname, String code) {
         boolean failed = false;
         compile(fullname, code);
 
         programOutput = new ByteArrayOutputStream(32768);
         programErrors = new ByteArrayOutputStream();
 
-        PrintStream originalOutput = System.out;
-        PrintStream originalError = System.err; // Save original System.err
+        PrintStream originalOut = System.out;
+        PrintStream originalErr = System.err;
+        InputStream originalIn = System.in;
+
+
         Class<?> klass;
         try {
             System.setOut(new PrintStream(programOutput));
-            System.setErr(new PrintStream(originalError));
-            System.setIn(input);
+            System.setErr(new PrintStream(programErrors));
 
             String[] args = new String[]{};
             klass = manager.getClassLoader(null).loadClass(fullname);
             klass.getDeclaredMethod("main", String[].class)
                 .invoke(null, (Object)args);
-        } catch (Exception e) {
+        } 
+        catch (Exception e) {
             failed = true;
+            e.printStackTrace(new PrintStream(programErrors));
         }
         finally {
-            System.setOut(originalOutput);
-            System.setErr(originalError);
-            System.setIn(System.in);
+            System.setOut(originalOut);
+            System.setErr(originalErr);
+            System.setIn(originalIn);
+
+            
         }
         return failed;            
     }
 
+    public ByteArrayOutputStream getProgramOutput() {
+        return programOutput;
+    }
+
+    public ByteArrayOutputStream getProgramErrors() {
+        return programErrors;
+    }
+
+    public String getCompileErrors() {
+        return compilerOutput.toString();
+    }
+
+    public boolean updateInput(CharSequence input) {
+        try {
+            programInput = new ByteArrayInputStream(input.toString().getBytes());
+            System.setIn(programInput);
+        }
+        catch (Exception e) {
+            System.setOut(System.out);
+            System.setErr(System.err);
+            System.err.println("Failed to update input");
+            return false;
+        }
+        
+        return true;
+    }
 
     public CodeExecution() {
        compiler = ToolProvider.getSystemJavaCompiler();
-       manager = new CompilationManager(compiler.getStandardFileManager(null, null, null));
+       manager = new CompilationManager<>(compiler.getStandardFileManager(null, null, null));
     }
 }
 
@@ -142,16 +168,34 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello, from Visual Studio Code!");
         String sourceCode = """
-            public lass HelloWorld {
-                public static void main(String[] args) {
-                    System.out.println("Hello, World!");
-                    System.err.println("This is an error message.");
-                }
 
-                public void cock() {}
+            import java.io.*;
+            import java.util.*;
+
+            public class Dang {
+                public static void main(String[] args) {
+                    Scanner input = new Scanner(System.in);
+                    System.out.println("Hello, World!");
+                    System.out.println("This is an error message.");
+                    System.out.println(input.nextLine());
+                    System.out.println(input.nextLine());
+                    System.out.println(input.nextInt() + input.nextInt());
+                    System.out.println(input.nextInt());
+                }
             }
         """;
         CodeExecution executor = new CodeExecution();
-        executor.execute(null, "HelloWorld", sourceCode);
+        executor.updateInput("cock\nball 3 2\n3 1\npenis");
+
+        boolean failed = executor.execute("Dang", sourceCode);
+        if (failed) {
+            System.out.println("Compilation or execution error:");
+            System.out.println("Compile Errors: " + executor.getCompileErrors());
+            System.out.println("Runtime Errors: " + executor.getProgramErrors().toString());
+        } else {
+            System.out.println("Program Output:");
+            System.out.println(executor.getProgramOutput().toString());
+        }
+        
     }
 }
